6376980231771850146426c561514db0154909f3
savanahatieno <savanaht01@gmail.com>
2021-03-08 21:58:37 +0300

    Set the input in the js file

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('.summary').hide();
+    $('.cdata-overlay').hide();
+
+
+/*=======Input Section=======*/
+    $('checkout').click(function(){
+        let type = $('.type option: selected') .val();
+        let size = $('.type option: selected') .val();
+        let crust = $('.type option: selected') .val();
+        let topping = $('.type option: selected') .val();
+        let number = $('#number') .val();
+        console.log(size);
+
+
+
+
+
+        
+    });
