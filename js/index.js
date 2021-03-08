fa3d2a099179e5f64e36d0dd787dafde87b3a796
savanahatieno <savanaht01@gmail.com>
2021-03-08 22:09:43 +0300

    set an order function

--- js/index.js
+++ js/index.js
@@ -5,16 +5,19 @@ $(function () {
 
 /*=======Input Section=======*/
     $('checkout').click(function(){
-        let type = $('.type option: selected') .val();
-        let size = $('.type option: selected') .val();
-        let crust = $('.type option: selected') .val();
-        let topping = $('.type option: selected') .val();
+        let type = $('#type option: selected') .val();
+        let size = $('#size option: selected') .val();
+        let crust = $('#crust option: selected') .val();
+        let topping = $('#toppings option: selected') .val();
         let number = $('#number') .val();
         console.log(size);
 
+        /*Order Function*/
+        let order = (t, s, c, t, n, total) => {
+            return {t, s, c, t, n, total};
+        };
 
 
 
 
-        
     });
